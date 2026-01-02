9bf9c4ba8c72771645b32e0ef629b0875842b376
deb0pam <debopamdas8@gmail.com>
2026-01-02 16:31:13 +0530

    autologin added

--- .config/suckless/b.sh
+++ .config/suckless/b.sh
@@ -5,7 +5,7 @@ do
    ip=$(ping -c 1 google.com > /dev/null 2>&1 && echo "Connected" || echo "Disconnected")
    vol="$(pactl get-sink-volume 0 | awk -F '/' '{print $2}' | grep -o '[0-9]\+')%"
    brightness=$(xrandr --verbose | grep -i brightness | awk '{print $2 * 100 "%"}')
-   t=$(echo "Storage: $(df -h / | awk '/\// {print $3 "/" $2 " GB"}') RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') CPU: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4"%"}')")
-   xsetroot -name "$t vol:$vol bt:$battery $b bri:$brightness $ip $(date +"%A %d.%m.%y %H:%M:%S")"
+   t=$(echo "Storage: $(df -h / | awk '/\// {print $4 "/" $2 " "}') RAM: $(free -h | awk '/Mem:/ {print $3 "/" $2}') CPU: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4"%"}')")
+   xsetroot -name "$t vol:$vol bri:$brightness bat:$battery $b $ip $(date +"%A %d.%m.%y %H:%M:%S")"
    sleep 0.5
 done
